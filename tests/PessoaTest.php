5e50b1c842bddeebfbac0b0e6f617e61189ae809
Wilson Glasser <wilsonglasser@gmail.com>
2020-01-17 01:11:44 -0300

    Create PessoaTest.php

---
+++ tests/PessoaTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace PhpBoleto\Tests;
+
+use PhpBoleto\Something;
+
+class PessoaTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSomething()
+    {
+        $this->something = new Something;
+        // $this->assertEquals($expected, $actual);
+    }
+}
+
