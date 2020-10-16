0dd53db7077e7112f2c430b9ec9690c985bc1f33
Wilson Glasser <wilsonglasser@gmail.com>
2020-10-16 02:33:13 -0300

    retirado extensão intl

--- src/Service/AbstractService.php
+++ src/Service/AbstractService.php
@@ -9,8 +9,5 @@ namespace WilsonGlasser\PhpBoleto\Service;
  */
 abstract class AbstractService {
 
-    public function ascii($string)
-    {
-        return preg_replace('/[`^~\'"]/', null, str_replace('?', '', iconv('UTF-8', 'ASCII//TRANSLIT', $string)));
-    }
+
 }
